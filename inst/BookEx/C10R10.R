efbc09d7315df45e90597d0aa9e43a006e3124b3
bpfaff <bernhard@pfaffikus.de>
2016-12-07 21:44:30 +0100

    Replaced examples for robust optimization from 1st ed with 2nd

--- inst/BookEx/C10R10.R
+++ inst/BookEx/C10R10.R
@@ -1,11 +1,27 @@
 ## Efficient Frontier
-EF <- rbind(PmrAns,
-            PmvAns[, c(1, 2)])
-plot(EF, type = "l", xlim = c(0, 8), ylim = c(0, 1.5),
-     ylab = "Portfolio Return", xlab = "Portfolio Risk", lwd = 2)
-points(RobAns[, c(1,2)], col = "blue", pch = 19, cex = 1.2)
-points(mveq[1], mveq[2], col = "red", pch = 19, cex = 1.2)
-legend("topleft", legend = c("Efficient Frontier",
-       "Robust Portfolio Solutions", "Equivalent MV-Portfolio"),
-       lty = c(1, NA, NA), pch = c(NA, 19, 19),
-       col = c("black", "blue", "red"), lwd = 2)
+## determining ranges
+xlims <- c(SigMin * 0.9,
+           max(cbind(MVans[, 1], RCans[, 1])))
+ylims <- c(min(cbind(MVans[, 2], RCans[, 2])),
+           max(cbind(MVans[, 2], RCans[, 2])))
+## plotting efficient frontier for MV
+plot(MVans[, 1], MVans[, 2], type = "l",
+     xlim = xlims,
+     ylim = ylims,
+     xlab = expression(sigma),
+     ylab = expression(mu))
+## Superimposing points
+for(i in 1:nrow(MVans)){
+    points(x = MVans[i, 1], y = MVans[i, 2], col = "blue", pch = 19)
+    points(x = RCans[i, 1], y = RCans[i, 2], col = "red", pch = 19)
+}
+## Superimposing equivalence points
+points(x = rceq[1], y = rceq[2], col = "darkgreen", bg = "darkgreen", pch = 23)
+points(x = mveq[1], y = mveq[2], col = "green", bg = "green", pch = 23)
+## Legend
+legend("topleft", legend = c("Efficient Frontier", "MV points", "RC points",
+                      expression(paste("RC allocation with ", theta == 0.7)),
+                      "Equivalent MV-Portfolio"),
+       lty = c(1, NA, NA, NA, NA), pch = c(NA, 19, 19, 23, 23),
+       pt.bg = c(NA, NA, NA, "darkgreen", "orange"),
+       col = c("black", "blue", "red", "darkgreen", "orange"))
--- inst/BookEx/C10R8.R
+++ inst/BookEx/C10R8.R
@@ -1,22 +1,30 @@
-PMV <- function(SRoot, mu, SigMax, lambda, ...){
-  Nassets <- length(mu)
-  ra <- 1 - lambda
-  f <- -mu
-  C1 <- matrix(rep(0, Nassets), nrow = 1)
-  ## no cone, but quadratic constraint
-  C2 <- diag(Nassets)
-  ## non-negativity constraint
-  C3 <- matrix(rep(-1, Nassets), nrow = 1)
-  ## budget constraint
-  C <- rbind(C1,
-           C2,
-           C3)
-  ZeroM <- matrix(0, nrow = Nassets + 1, ncol = Nassets)
-  A <- rbind(SRoot,
-             ZeroM)
-  b <- rep(0, nrow(A))
-  d <- c(SigMax * ra, rep(0, Nassets), 1)
-  ans <- Socp(f = f, A = A, b = b, C = C, d = d,
-              N = c(nrow(SRoot), rep(1, nrow(ZeroM))), ...)
-  return(ans)
+library(cccp)
+## Setting of parameters
+Nassets <- ncol(rzoo)
+Nobs <- nrow(rzoo)
+mu <- colMeans(rzoo)
+S <- cov(rzoo)
+SR <- sqrm(S)
+delta <- sqrt(qchisq(0.9, Nassets))
+## Determining feasible risk aversion
+SigMax <- max(colSds(rzoo))
+SigMin <- min(colSds(rzoo))
+ra <- seq(SigMin * 1.1, SigMax * 0.9, length.out = 10) / SigMax
+## Initializing objects for MV and robust counterpart results
+RCans <- MVans <- matrix(NA,
+                         nrow = 10,
+                         ncol = Nassets + 2)
+## Computing points on efficient frontier and allocations
+for(i in 1:10){
+    ## minimum-variance
+    wmv <- PMV(SRoot = SR, mu = mu, SigTerm = SigMin / ra[i])
+    MVans[i, ] <- c(sqrt(t(wmv) %*% S %*% wmv),
+                     crossprod(mu, wmv),
+                     wmv)
+    ## robust counterpart
+    theta <- ra[i] + (1 - ra[i]) * delta / sqrt(Nobs)
+    wrc <- PMV(SRoot = SR, mu = mu, SigTerm = SigMin / theta)
+    RCans[i, ] <- c(sqrt(t(wrc) %*% S %*% wrc),
+                     crossprod(mu, wrc),
+                     wrc)
 }
